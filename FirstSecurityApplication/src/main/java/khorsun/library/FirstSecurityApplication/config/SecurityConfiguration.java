6601bcca9dd0d80603effa60b8da4cd9f0a5636f
mishgan55 <vorota.mogilev@gmail.com>
2023-08-03 18:40:43 +0300

    Created Custom login page

--- FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
+++ FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 //здесь будет проводить вся конфигурация нашего секьюрити(аунтефикация и авторизация)
 //Ставим обязательно аннотацию над классом @EnableWebSecurity
@@ -23,6 +25,22 @@ public class SecurityConfiguration {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()// не аутефицированный пользователь
+                .antMatchers("/auth/login","/error").permitAll()// пускаем его на эту страницу
+                .anyRequest().authenticated()//для всех других запросов пользователь должен быть аутенфицирован
+                .and()
+                .formLogin() // форма для логина
+                .loginPage("/auth/login")// по какой ссылке переходим
+                .loginProcessingUrl("/process_login")// указываем на какую сслыку переходим при сабмите формы
+                .defaultSuccessUrl("/show",true)//после успешной аунтефикации перенапрваление на страницу
+                .failureUrl("/auth/login?error");// при неудачной попытке входа
+
+        return http.build();
     }
 
     @Bean
