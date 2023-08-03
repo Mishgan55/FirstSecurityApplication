c4bc85c45b2fc946250b2f2d0f812bde3927168d
mishgan55 <vorota.mogilev@gmail.com>
2023-08-03 16:54:19 +0300

    Delete class AuthProviderImpl and simplify my security app

--- FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
+++ FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
@@ -1,23 +1,32 @@
 package khorsun.library.FirstSecurityApplication.config;
 
-import khorsun.library.FirstSecurityApplication.security.AuthProviderImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //здесь будет проводить вся конфигурация нашего секьюрити(аунтефикация и авторизация)
 //Ставим обязательно аннотацию над классом @EnableWebSecurity
 // и наследуемся от класса WebSecurityConfiguration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
-    @Autowired
-    public SecurityConfiguration(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+@Configuration
+public class SecurityConfiguration {
+
+    /*In the old version you inject AuthenticationManagerBuilder, set userDetailsService,
+     passwordEncoder and build it. But authenticationManager is already created in this step.
+     It is created the way we wanted (with userDetailsService and the passwordEncoder).*/
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+            throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    protected void configure(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(authProvider);
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
