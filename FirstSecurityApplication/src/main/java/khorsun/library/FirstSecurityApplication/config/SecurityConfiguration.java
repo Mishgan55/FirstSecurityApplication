282f0dcd48585a9e5e03805c855469d7d56406b8
mishgan55 <vorota.mogilev@gmail.com>
2023-08-04 16:37:42 +0300

    Added BCryptoEncoder

--- FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
+++ FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -49,6 +50,6 @@ public class SecurityConfiguration {
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/services/SecurityService.java
+++ FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/services/SecurityService.java
@@ -3,18 +3,22 @@ package khorsun.library.FirstSecurityApplication.services;
 import khorsun.library.FirstSecurityApplication.models.Person;
 import khorsun.library.FirstSecurityApplication.repositories.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SecurityService {
 
+    private final PasswordEncoder passwordEncoder;
     private final PersonRepository personRepository;
     @Autowired
-    public SecurityService(PersonRepository personRepository) {
+    public SecurityService(PasswordEncoder passwordEncoder, PersonRepository personRepository) {
+        this.passwordEncoder = passwordEncoder;
         this.personRepository = personRepository;
     }
 
     public void registration(Person person){
+         person.setPassword(passwordEncoder.encode(person.getPassword()));
          personRepository.save(person);
     }
 }
