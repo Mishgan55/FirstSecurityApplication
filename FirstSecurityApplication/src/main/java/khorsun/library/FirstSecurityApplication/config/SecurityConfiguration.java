6a294fbfd25cfd78c5992b560546ce6edb490cf1
mishgan55 <vorota.mogilev@gmail.com>
2023-08-04 17:04:16 +0300

    Added CSRF token for forms

--- FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
+++ FirstSecurityApplication/src/main/java/khorsun/library/FirstSecurityApplication/config/SecurityConfiguration.java
@@ -30,7 +30,7 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()//отключение csrf токена
+            http
                 .authorizeRequests()// не аутефицированный пользователь
                 .antMatchers("/auth/login","/auth/registration","/error").permitAll()// пускаем его на эту страницу
                 .anyRequest().authenticated()//для всех других запросов пользователь должен быть аутенфицирован
